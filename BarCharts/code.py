4b6da77823291f4d2bd9755f86a56b01993787af
trojancode95 <akhileshecenitjsr@gmail.com>
2021-05-25 20:40:26 +0530

    barchart practice

--- BarCharts/code.py
+++ BarCharts/code.py
@@ -1,12 +1,38 @@
+import csv
 import numpy as np
+import pandas as pd
+from collections import Counter
 from matplotlib import pyplot as plt
 
 plt.style.use("fivethirtyeight")
 
+data = pd.read_csv('data.csv')
+ids = data['Responder_id']
+lang_responses = data['LanguagesWorkedWith']
 
-plt.title("Median Salary (USD) by Age")
-plt.xlabel("Ages")
-plt.ylabel("Median Salary (USD)")
+language_counter = Counter()
+
+for response in lang_responses:
+    language_counter.update(response.split(';'))
+
+languages = []
+popularity = []
+
+for item in language_counter.most_common(15):
+    languages.append(item[0])
+    popularity.append(item[1])
+
+# print(languages)
+# print(popularity)
+
+languages.reverse()
+popularity.reverse()
+plt.barh(languages, popularity)
+
+
+plt.title("Most Popular Languages")
+# plt.xlabel("Programming Languages")
+plt.xlabel("Number of people who Use")
 
 plt.tight_layout()
 
